8fa31152891b209e8551e5121d03c1754a0eb24f
Stuart Murray <stuart.w.murray@gmail.com>
2023-02-13 22:33:00 +1100

    Removed some unused types and replace a BaseDefinition with Definition.

--- packages/typeytypetype/src/definitions/internal/types.ts
+++ packages/typeytypetype/src/definitions/internal/types.ts
@@ -1,28 +1,14 @@
 import { type TypeFromDefinition } from '../../types.js';
-import { type BaseDefinition } from '../BaseDefinition.js';
+import { type Definition } from '../Definition.js';
 
 export type MappedTypeFromDefinition<T> = {
     [TKey in keyof T]: TypeFromDefinition<T[TKey]>;
 };
 
 export type MappedDefinition<T> = {
-    [TKey in keyof T]: BaseDefinition<T[TKey]>;
+    [TKey in keyof T]: Definition<T[TKey]>;
 };
 
 export type SpreadDefinition<TUnion> = TUnion extends any
-    ? BaseDefinition<TUnion>
+    ? Definition<TUnion>
     : never;
-
-export type Unspecialize<T extends BaseDefinition<unknown>> =
-    T extends BaseDefinition<infer U> ? BaseDefinition<U> : T;
-
-export type UnspecializeObject<
-    T extends Record<string, BaseDefinition<unknown>>
-> = {
-    [K in keyof T]: Unspecialize<T[K]>;
-};
-export type UnspecializeArray<
-    T extends ReadonlyArray<BaseDefinition<unknown>>
-> = {
-    [K in keyof T]: Unspecialize<T[K]>;
-};
