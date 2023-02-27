b824500b4712fb213e86277d6330ebc9b28fe1ad
Stuart Murray <stuart.w.murray@gmail.com>
2023-02-27 22:08:02 +1100

    Shared eslint file..

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'standard-with-typescript',
+        'prettier',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        // Fix up typescript imports to have .js
+        'plugin:require-extensions/recommended',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    rules: {
+        'typescript-eslint/promise-function-async': ['Off'],
+        'n/no-callback-literal': ['Off']
+    },
+    settings: {
+        'import/resolver': {
+            typescript: true,
+            node: true,
+        },
+    },
+    plugins: ['require-extensions'],
+};
